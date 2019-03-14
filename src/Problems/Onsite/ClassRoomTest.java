8b36d11ff2df4c176b0fafd139b134dd90ee73d4
Shuhan-Liu <904711775@qq.com>
2019-03-13 22:02:30 -0700

    Practiced two more problems
    1. ClassRoom: User PriorityQueue and define interval exclusively
    2. HashMapWithExpire: define two hashmaps, one for value, one for time

---
+++ src/Problems/Onsite/ClassRoomTest.java
@@ -0,0 +1,145 @@
+package Problems.Onsite;
+
+import java.util.*;
+
+/**
+ * Created by shuhanliu on 3/13/19.
+ *
+ * LC855 考试找位子，尽量分散坐，人会离开
+ *
+ * 思路：见leetcode
+ * 
+ * 1.用优先队列：用优先队列存slot，slot包含左右端点和长度。exclusive好算。
+ * 注意如果是最左或最右时长度为right - left，若非则为(right - left) /
+ *
+ * 2，因为如果选择坐边上可以不管端点。seat时候去pq最大slot，中间切开offer两段。
+ * leave时候遍历找到左右两段整合
+ */
+public class ClassRoomTest {
+
+    public static void main(String[] args) {
+        ExamRoom examRoom = new ExamRoom(4);
+//        examRoom.printSeats();
+
+        System.out.println(examRoom.seat());
+//        examRoom.printSeats();
+
+        System.out.println(examRoom.seat());
+//        examRoom.printSeats();
+
+        System.out.println(examRoom.seat());
+//        examRoom.printSeats();
+
+        System.out.println(examRoom.seat());
+//        examRoom.printSeats();
+
+        examRoom.leave(1);
+//        examRoom.printSeats();
+
+        examRoom.leave(3);
+//        examRoom.printSeats();
+
+        System.out.println(examRoom.seat());
+//        examRoom.printSeats();
+    }
+
+    static class ExamRoom {
+
+        PriorityQueue<Interval> pq;
+        int N;
+
+        class Interval {
+            int s;
+            int e;
+            int dist;
+
+            public Interval(int s, int e) {
+                this.s = s;
+                this.e = e;
+
+                if (s == -1) {
+                    this.dist = e;
+                } else if (e == N) {
+                    this.dist = N-s-1;
+                } else {
+                    this.dist = (e-s)/2;
+                }
+            }
+        }
+
+        public ExamRoom(int N) {
+            this.N = N;
+            pq = new PriorityQueue<>((a, b) -> a.dist != b.dist ? b.dist-a.dist : a.s-b.s);
+            pq.offer(new Interval(-1, N));
+        }
+
+        public int seat() {
+            Interval interval = pq.poll();
+            int seat;
+            if (interval.s == -1) {
+                seat = 0;
+            } else if (interval.e == N) {
+                seat = N-1;
+            } else {
+                seat = (interval.s + interval.e)/2;
+            }
+
+            pq.offer(new Interval(interval.s, seat));
+            pq.offer(new Interval(seat, interval.e));
+            return seat;
+        }
+
+        public void leave(int p) {
+            Interval first = null;
+            Interval second = null;
+
+            List<Interval> list = new ArrayList<>(pq);
+            for (Interval inter : list) {
+                if (inter.s == p) second = inter;
+                else if (inter.e == p) first = inter;
+            }
+
+            pq.remove(first);
+            pq.remove(second);
+
+            pq.offer(new Interval(first.s, second.e));
+        }
+    }
+}
+
+
+/**
+ * while (left < right && seats[left] == 0)
+ left++;
+ while (left < right && seats[right] == 0)
+ right--;
+
+ int maxDistance = left;
+ int minIndex = left;
+ int prevSeatedIndex = left;
+
+ for (int i = left; i <= right; i++) {
+ if (seats[i] == 1) {
+ if ((i-prevSeatedIndex)/2-1 > maxDistance) {
+ maxDistance = (i-prevSeatedIndex)/2-1;
+ minIndex = prevSeatedIndex;
+ }
+ prevSeatedIndex = i;
+ }
+ }
+
+ maxDistance = Math.max(maxDistance, Math.max(left, seats.length-1-right-1));
+
+ if (maxDistance == left) {
+ seats[0] = 1;
+ count++;
+ return 0;
+ } else if (maxDistance == seats.length-1-right-1) {
+ seats[seats.length-1] = 1;
+ count++;
+ return seats.length-1;
+ } else {
+ seats[minIndex + maxDistance+1] = 1;
+ count++;
+ return minIndex + maxDistance+1;
+ }*/
---
+++ src/Problems/Onsite/HashMapWithExpire.java
@@ -0,0 +1,83 @@
+package Problems.Onsite;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by shuhanliu on 3/13/19.
+ *
+ * Create a map with expiring entries:
+ * 思路：两个hash map，一个记录key，value pair，一个记录key的过期时间，
+ * get的时候检查key是否过期，如果过期了，删除key返回null
+ * Put方法有三个参数，除了key，value还有个duration
+ *
+ * Follow up: 采用更主动的策略删除过期的Key
+ * 思路；创建后台线程定期清理过期的Key。
+ * 用两个map，一个装<key, value>一个装<key, expiredTime>
+ * 在get中采用lazy deletion，get的时候检查key是否过期，如果过期的话两个map中都删除key，返回null。
+ * put的时候每次都更新key的expiredTime。
+ * 后台线程每过一段时间遍历所有key，调用get方法删除过期key。此处为了避免多线程冲突，Map用ConcurrentHashMap实现。
+ *
+ */
+public class HashMapWithExpire {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExpireMap<Integer, Integer> expireMap = new ExpireMap<>();
+        System.out.println(expireMap.get(0));
+        expireMap.put(0, 1, 1000L);
+        Thread.sleep(500L);
+        System.out.println(expireMap.get(0));
+        Thread.sleep(500L);
+        System.out.println(expireMap.get(0));
+    }
+
+    static class ExpireMap<K, V> {
+        Map<K, V> valueMap;
+        Map<K, Long> timeMap;
+
+        private Thread cleanThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try{
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    for (K key : timeMap.keySet())
+                        get(key);
+                }
+            }
+        });
+
+        public ExpireMap() {
+            valueMap = new ConcurrentHashMap();
+            timeMap = new ConcurrentHashMap();
+            cleanThread.start();
+        }
+
+        public V get(K key) {
+
+            if (!valueMap.containsKey(key))
+                return null;
+
+            Long curTime = System.currentTimeMillis();
+
+            if (curTime > timeMap.get(key)) {
+                valueMap.remove(key);
+                timeMap.remove(key);
+                return null;
+            } else {
+                return valueMap.get(key);
+            }
+        }
+
+        public void put(K key, V value, Long expireIn) {
+            Long curTime = System.currentTimeMillis();
+            valueMap.put(key, value);
+            timeMap.put(key, curTime + expireIn);
+        }
+    }
+}
